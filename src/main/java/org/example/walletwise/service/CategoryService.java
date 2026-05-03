6d22e772a2c1ba6202fcb1365fcea791a979cd68
TM4tey <t.mater0517@gmail.com>
2026-05-03 21:45:29 +0200

    Implement services

---
+++ src/main/java/org/example/walletwise/service/CategoryService.java
@@ -0,0 +1,22 @@
+package org.example.walletwise.service;
+
+import org.example.walletwise.model.Category;
+import org.example.walletwise.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+}
---
+++ src/main/java/org/example/walletwise/service/ExpenseService.java
@@ -0,0 +1,31 @@
+package org.example.walletwise.service;
+
+import org.example.walletwise.model.Expense;
+import org.example.walletwise.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+
+    public List<Expense> findAll() {
+        return expenseRepository.findAll();
+    }
+
+    public Optional<Expense> findById(Long id) {
+        return expenseRepository.findById(id);
+    }
+
+    public Expense save(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    public void deleteById(Long id) {
+        expenseRepository.deleteById(id);
+    }
+}
