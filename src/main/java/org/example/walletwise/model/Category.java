146091e5785191389025e9ba8c46fc09e513e4d8
TM4tey <t.mater0517@gmail.com>
2026-05-03 21:08:21 +0200

    Add category and repo interface

---
+++ src/main/java/org/example/walletwise/model/Category.java
@@ -0,0 +1,22 @@
+package org.example.walletwise.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "A név nem lehet üres!")
+    private String name;
+}
