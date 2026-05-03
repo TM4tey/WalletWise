40b6ae50d9cbd208fcece6c202cc74b10415277b
TM4tey <t.mater0517@gmail.com>
2026-05-03 20:50:52 +0200

    initial commit

---
+++ src/test/java/org/example/walletwise/WalletWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.walletwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
